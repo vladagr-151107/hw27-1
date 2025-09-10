607f8aa127fbe67ec5743f2d28f3292882ea4ef4
Vlada Grushchenko <vladagrushchenko@gmail.com>
2025-09-10 19:30:57 +0300

    did toggle switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -8,14 +7,22 @@ import { Routes, Route} from 'react-router-dom';
 import "./styles.css";
 
 function App(){
-    return <div>
-        <Header/>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/contacts" element={<Contacts/>}/>
-        </Routes>
-    </div>
+
+    const [isDark, setDark] = useState(false);
+    const toggleTheme = () => {
+        setDark(prev => !prev);
+    };
+
+    return (
+        <div className={isDark ? 'dark' : 'light'}>
+            <Header toggleTheme={toggleTheme} />
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contacts" element={<Contacts/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Header(){
+function Header({ toggleTheme}){
     return (<header>
         <nav>
             <Link to="/" class="link">Головна</Link>
             <Link to="/about" class="link">Про мене</Link>
             <Link to="/contacts" class="link">Контакти</Link>
+            <button id="toggle-theme" className="btn btn-outline-primary" onClick={toggleTheme}> Dark mode </button>
         </nav>
     </header>);
 }
