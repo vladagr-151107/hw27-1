6bc70e54babf904573026035ef89d54645b67174
Vlada Grushchenko <vladagrushchenko@gmail.com>
2025-09-10 19:36:00 +0300

    todolist redone. point 5 left

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Home(){
-    return(
+function Home() {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (inputValue.trim()) {
+            setTodos([...todos, { id: Date.now(), text: inputValue }]);
+            setInputValue("");
+        }
+    };
+
+    const deleteTodo = (id) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    };
+
+    return (
         <div>
-    <div class="container py-4">
-    <h1 class="mb-4 text-center">ToDoList</h1>
-    <form class="form js--form d-flex gap-2 mb-4">
-        <input type="text" name="value" required class="form__input js--form__input form-control"/>
-        <button class="form__btn btn btn-success">Add</button>
-    </form>
-    <ul class="js--todos-wrapper list-unstyled"></ul>
-</div>
-
-<div class="modal fade" id="taskModal" tabindex="-1" aria-labelledby="taskModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="taskModalLabel">Ваше завдання</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрити"></button>
-      </div>
-      <div class="modal-body" id="modalTaskText"></div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрити</button>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-  );
+            <div className="container py-4">
+                <h1 className="mb-4 text-center">ToDoList</h1>
+                <form className="form d-flex gap-2 mb-4" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        required
+                        className="form__input form-control"
+                    />
+                    <button className="form__btn btn btn-success">Add</button>
+                </form>
+                <ul className="list-unstyled">
+                    {todos.map(todo => (
+                        <li key={todo.id} className="todo-item">
+                            <span className="todo-item__description">{todo.text}</span>
+                            <button 
+                                className="todo-item__delete"
+                                onClick={() => deleteTodo(todo.id)}
+                            >
+                                Delete
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
